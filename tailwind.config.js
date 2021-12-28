28bb4e42eb026ca7a2b54becbe544bc6dbf6d0af
wmoralesdev <walterrafael26@gmail.com>
2021-12-28 15:24:19 -0600

    refactor: Linted

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     colors: {
       primary: '#23272C',
       accent: '#ED554C',
-      secondary : '#033C71',
+      secondary: '#033C71',
     },
     fontFamily: {
-      'sans':['Montserrat', 'sans-serif'],
+      sans: ['Montserrat', 'sans-serif'],
     },
     extend: {},
   },
   plugins: [],
-}
+};
