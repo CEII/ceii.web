f30836560665a0520a3460c6265f1ce70eb9321c
MarianaLagrava <00219820@uca.edu.sv>
2021-12-28 14:10:02 -0600

    feat: Configured theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      primary: '#23272C',
+      accent: '#ED554C',
+      secondary : '#033C71',
+    },
     extend: {},
   },
   plugins: [],
