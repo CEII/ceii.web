68b17feece30114499c9b779c9f443737a5566cb
MJZF00155619 <00155619@uca.edu.sv>
2022-01-03 17:14:20 -0600

    style:Added spacing variation and fix md / lg size

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,10 +4,10 @@ import { faFacebookSquare, faInstagram, faGithubSquare, faDiscord } from "@forta
 
 const Footer : FunctionComponent = () => {
     return (
-        <footer className="flex justify-between p-8">
+        <footer className="flex justify-between footer-items-space">
             <div className="flex bg-white rounded-full">
                 <a href="https://www.facebook.com/CEII.UCA" className="self-center">
-                    <FontAwesomeIcon icon={faFacebookSquare} color="#033C71" className="social-icon-p"/>
+                    <FontAwesomeIcon icon={faFacebookSquare} color="#033C71" className="social-icon-p sm:ml-3 md:ml-4 lg:ml-8"/>
                 </a>
                 <a href="https://www.instagram.com/ceii.uca/?hl=es-la" className="self-center">
                     <FontAwesomeIcon icon={faInstagram} color="#033C71" className="social-icon-p"/>
@@ -16,7 +16,7 @@ const Footer : FunctionComponent = () => {
                     <FontAwesomeIcon icon={faGithubSquare} color="#033C71" className="social-icon-p"/>
                 </a>
                 <a href="https://discord.gg/MAYVjVyZcC" className="self-center">
-                    <FontAwesomeIcon icon={faDiscord} color="#033C71" className="social-icon-p mr-6"/>
+                    <FontAwesomeIcon icon={faDiscord} color="#033C71" className="social-icon-p sm:mr-3 md:mr-4 lg:mr-8"/>
                 </a>
             </div>
             <p className="text-white font-sans font-bold self-center copyright-sizes">© CEII - 2021</p>
