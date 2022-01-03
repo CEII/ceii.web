448e433d559ba2d7db2cf8b923207039bb2b67c0
00190220 <00190220@uca.edu.sv>
2022-01-02 23:00:37 -0600

    fix: svgs, buttons, type-page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,28 +1,22 @@
-import Image from 'next/image';
+import LostFido from 'components/Svgs/LostFido';
+import { NextPage } from 'next';
 
-const Custom404 = () => {
+const Custom404: NextPage = () => {
   return (
     <div className="bg-primary p-16 w-full h-screen  ">
       <div className="flex flex-col mt-24 space-y-3 justify-center items-center lg:mt-40">
         <div
-          className=" bg-white text-xl font-bold w-1/3 text-center rounded-lg 
+          className=" bg-white text-xl font-bold w-1/3 text-center rounded-full
           lg: w-24 "
         >
           <h1 className="text-accent">404</h1>
         </div>
         <p className="text-white font-bold text-lg">No hay nada por aquí...</p>
         <div className="w-auto">
-          <Image
-            className="
-            lg:"
-            src="/lost-fido.svg"
-            width={250}
-            height={250}
-            priority
-          />
+          <LostFido />
         </div>
         <button
-          className=" w-9/12  font-semibold bg-accent text-white rounded-md
+          className=" w-9/12  font-semibold bg-accent text-white rounded-full
           md:w-40 
           lg:w-48  
           
