9e2770155324a2d415f3ab254cfe5440062401a5
MarianaLagrava <00219820@uca.edu.sv>
2022-01-03 20:31:05 -0600

    fix: Deleted  Errorpages

--- pages/ErrorPages/index.tsx
+++
@@ -1,9 +0,0 @@
- import type { NextPage } from 'next';
-
- const ErrorPages: NextPage = () => (
-         <div>
-             <h1>pagina de error ruta: /404</h1>
-         </div>
- )
-
- export default ErrorPages;
