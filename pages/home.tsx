981f1fbd3005911e832a40dfca3213f2a68c4efb
fernanortega-hub <00191820@uca.edu.sv>
2022-01-04 17:53:24 -0600

    feat: Add responsive for md and lg

--- pages/home.tsx
+++ pages/home.tsx
@@ -3,23 +3,23 @@ import ConstructionFido from '@components/ConstructionFido/ConstructionFido';
 
 const Home: NextPage = () => {
     return (
-        <div className="bg-primary w-full h-screen">
-            <div className="flex flex-col justify-center items-center">
-                <span className="bg-white text-accent px-5 text-3xl font-bold rounded-full mt-24 mb-6">
+        <div className="bg-primary w-full h-screen flex justify-center">
+            <div className="flex flex-col justify-center items-center space-y-5">
+                <span className="bg-white py-1 text-accent px-5 text-3xl font-bold rounded-full md:text-4xl md:px-6">
                     Ups!
                 </span>
-                <span className="text-white font-bold text-2xl mb-5">
+                <span className="text-white font-bold text-2xl break-words md:text-3xl">
                     Estamos en construcción
                 </span>
                 <div className="w-auto">
-                    <ConstructionFido className="h-64" />
+                    <ConstructionFido className="h-64 md:h-72" />
                 </div>
-                <span className="text-white italic text-2xl text-center mt-5 mb-6">
+                <p className="text-white italic text-2xl text-center break-words w-96 md:text-3xl">
                     ¿Buscas los talleres socioafectivos?
-                </span>
+                </p>
                 <button
                     type="button"
-                    className="bg-accent text-white rounded-full px-4 py-2 text-lg font-bold"
+                    className="bg-accent text-white rounded-full px-4 py-2 text-lg font-bold md:text-2xl"
                 >
                     Sí, llévame allá
                 </button>
