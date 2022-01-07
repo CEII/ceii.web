16c8789c6ae9c45b238824d70d8bde4948522ed4
marcelob12 <00080119@uca.edu.sv>
2022-01-07 16:19:53 -0600

    style:struct and identation

--- pages/cms/index.tsx
+++ pages/cms/index.tsx
@@ -1,45 +1,48 @@
 import type { NextPage } from 'next';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 import Link from 'next/link';
 
 const Cms: NextPage = () => (
-        <div>
+        <div >
             <navar> 
-                <p className="bg-white"> espacio para el menu de barras</p> 
+                <FontAwesomeIcon icon={faBars}  className="w-10 ml-5 text-white "/> 
             </navar>
             
-            <section className="flex-col p-10 m-5 space-y-5">
+            <section className="flex-col m-8 space-y-5 ">
                 
-            <div className="text-white rounded-md bg-secondary">
+            <div className="p-3 text-white rounded-lg bg-secondary ">
                 <h2 className="font-sans font-semibold">Administracion de usuarios</h2>
-                <p className="font-light"> Envía invitaciones a nuevos usuarios, modifica la información y roles existentes.</p>
-                <Link href="/cms/users">
-                     <a className="rounded-full bg-accent">Vamos allá</a>
-                </Link>
+                    <p className="mt-3 text-xs font-light"> Envía invitaciones a nuevos usuarios, modifica la información y roles existentes.</p>
+                
+                <div className="flex justify-end pr-3">
+                    <Link href="/cms/users">
+                        <a className="pl-5 pr-5 mt-5 font-medium text-white rounded-full bg-accent">Vamos allá</a>
+                    </Link> 
+                </div>
             </div>
             
-            <div className="bg-white rounded-md text-secondary">
+            <div className="p-5 bg-white rounded-lg text-secondary">
                 <h2 className="font-sans font-semibold">Administracion de talleres sociofectivos</h2>
-                <p className="font-normal]"> Añade, modifica y elimina actividades. Consulta la información de un taller en específico</p>
-                <Link href="/cms/users">
-                     <a className="text-white rounded-full bg-accent">Vamos allá</a>
-                </Link>
+                    <p className="mt-3 text-xs font-light"> Añade, modifica y elimina actividades. Consulta la información de un taller en específico</p>
+                
+                <div className="flex justify-end pr-3">
+                    <Link href="/cms/users">
+                        <a className="pl-5 pr-5 m-3 font-medium text-white rounded-full bg-accent">Vamos allá</a>
+                    </Link> 
+                </div>
+                
             </div>
 
-            <div className="flex text-white rounded-md bg-secondary">
+            <div className="flex p-5 text-white rounded-lg bg-secondary">
                 <div> <p>logo ceii</p></div>
                 
                 <div>    
                 <h2 className="font-sans font-semibold">Es lo único que puedes hacer por el momento...</h2>
-                <p className="font-extralight">¡Agregaremos nuevas funciones pronto!</p>
-                <Link href="/cms/users">
-                     <a className="rounded-full bg-accent">Vamos allá</a>
-                </Link>
+                <p className="mt-3 text-xs font-light">¡Agregaremos nuevas funciones pronto!</p>
                 </div>
             </div>
-            
             </section>
-
-            
         </div>
 )
 export default Cms;
