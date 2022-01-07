a01aa7c1ab4a9f47746d774e1af4b11572a0ae6c
marcelob12 <00080119@uca.edu.sv>
2022-01-07 14:06:48 -0600

    feact:Basic desing to improve the cms page

--- pages/cms/index.tsx
+++ pages/cms/index.tsx
@@ -1,8 +1,45 @@
 import type { NextPage } from 'next';
+import Link from 'next/link';
 
 const Cms: NextPage = () => (
         <div>
-            <h1>administracion ruta:/cms</h1>
+            <navar> 
+                <p className="bg-white"> espacio para el menu de barras</p> 
+            </navar>
+            
+            <section className="flex-col p-10 m-5 space-y-5">
+                
+            <div className="text-white rounded-md bg-secondary">
+                <h2 className="font-sans font-semibold">Administracion de usuarios</h2>
+                <p className="font-light"> Envía invitaciones a nuevos usuarios, modifica la información y roles existentes.</p>
+                <Link href="/cms/users">
+                     <a className="rounded-full bg-accent">Vamos allá</a>
+                </Link>
+            </div>
+            
+            <div className="bg-white rounded-md text-secondary">
+                <h2 className="font-sans font-semibold">Administracion de talleres sociofectivos</h2>
+                <p className="font-normal]"> Añade, modifica y elimina actividades. Consulta la información de un taller en específico</p>
+                <Link href="/cms/users">
+                     <a className="text-white rounded-full bg-accent">Vamos allá</a>
+                </Link>
+            </div>
+
+            <div className="flex text-white rounded-md bg-secondary">
+                <div> <p>logo ceii</p></div>
+                
+                <div>    
+                <h2 className="font-sans font-semibold">Es lo único que puedes hacer por el momento...</h2>
+                <p className="font-extralight">¡Agregaremos nuevas funciones pronto!</p>
+                <Link href="/cms/users">
+                     <a className="rounded-full bg-accent">Vamos allá</a>
+                </Link>
+                </div>
+            </div>
+            
+            </section>
+
+            
         </div>
 )
 export default Cms;
