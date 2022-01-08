6e658786fc4ec974e5396dffc89e36e63f473d2c
fernanortega-hub <00191820@uca.edu.sv>
2022-01-07 22:24:07 -0600

    feat: adding responsive for md and lg

--- pages/team/index.tsx
+++ pages/team/index.tsx
@@ -1,24 +1,52 @@
 import type { NextPage } from 'next';
 
 const Team: NextPage = () => (
-    <div className="flex flex-col">
-        <div className="p-6 mt-5">
-            <h1 className="text-white font-bold text-2xl">
+    <div className="flex flex-col p-4 md:justify-start md:items-start md:px-48 lg:py-16 lg:px-32">
+        <div className="py-6 px-3 md:py-10">
+            <h1 className="text-white font-bold text-2xl md:text-xl lg:text-3xl">
                 Conoce a nuestros desarrolladores
             </h1>
-            <span className="text-white italic text-md">
+            <span className="text-white italic text-md md:text-base lg:text-xl">
                 ¡Gracias a todos por su contribución!
             </span>
         </div>
-        {/* Cards */}
-        <div className="space-y-4 p-3 flex flex-wrap text-white">
+        {/* Cards-container */}
+        <div className="flex flex-wrap text-white items-start md:justify-evenly">
             {/* Card */}
-            <div className="flex flex-col bg-accent p-3 rounded-xl">
-                <span className="text-bold text-base"> Walter Morales </span>
-                <span className="italic underline text-sm">
+            <div className="flex flex-col bg-accent p-2 rounded-xl m-3 md:p-4 lg:mx-2 lg:px-10">
+                <span className="font-semibold text-sm md:text-base lg:text-lg">
+                    Walter Morales
+                </span>
+                <span className="italic underline text-xs lg:text-sm">
                     00019618@uca.edu.sv
                 </span>
-                <ul className="flex flex-col text-sm mt-3">
+                <ul className="flex flex-col items-center text-xs mt-3 lg:text-sm">
+                    <li>Project Manager</li>
+                    <li>Scrum Master</li>
+                    <li>FullStack Developer</li>
+                </ul>
+            </div>
+            <div className="flex flex-col bg-accent p-2 rounded-xl m-3 md:p-4 lg:mx-2 lg:px-10">
+                <span className="font-semibold text-sm md:text-base lg:text-lg">
+                    Walter Morales
+                </span>
+                <span className="italic underline text-xs lg:text-sm">
+                    00019618@uca.edu.sv
+                </span>
+                <ul className="flex flex-col items-center text-xs mt-3 lg:text-sm">
+                    <li>Project Manager</li>
+                    <li>Scrum Master</li>
+                    <li>FullStack Developer</li>
+                </ul>
+            </div>
+            <div className="flex flex-col bg-accent p-2 rounded-xl m-3 md:p-4 lg:mx-2 lg:px-10">
+                <span className="font-semibold text-sm md:text-base lg:text-lg">
+                    Walter Morales
+                </span>
+                <span className="italic underline text-xs lg:text-sm">
+                    00019618@uca.edu.sv
+                </span>
+                <ul className="flex flex-col items-center text-xs mt-3 lg:text-sm">
                     <li>Project Manager</li>
                     <li>Scrum Master</li>
                     <li>FullStack Developer</li>
