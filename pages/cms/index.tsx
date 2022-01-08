f2e222f55358c8263d72c4e9edcfba00f06a0428
MarianaLagrava <00219820@uca.edu.sv>
2022-01-07 19:00:43 -0600

    feat: New sizes and ceii logo

--- pages/cms/index.tsx
+++ pages/cms/index.tsx
@@ -1,48 +1,52 @@
 import type { NextPage } from 'next';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 import Link from 'next/link';
+import Cmslogo from 'components/Cmslogo/Index';
 
 const Cms: NextPage = () => (
-        <div >
-            <navar> 
-                <FontAwesomeIcon icon={faBars}  className="w-10 mt-3 ml-3 text-2xl text-white"/> 
-            </navar>
-            
-            <section className="flex-col m-8 space-y-5 w-80">
-                
-            <div className="pl-3 text-sm text-white rounded-lg bg-secondary">
-                <h2 className="pt-2 font-sans font-semibold">Administracion de usuarios</h2>
-                    <p className="pt-2 space-y-5 text-xs font-extralight"> Envía invitaciones a nuevos usuarios, modifica la información y roles existentes.</p>
-                
+    <div>
+        <navar>
+            <FontAwesomeIcon icon={faBars} className="w-10 mt-3 ml-3 text-2xl text-white" />
+        </navar>
+
+        <section className="flex-col m-7 mt-16 space-y-5 w-80 p-2">
+
+            <div className="pl-4 text-sm text-white rounded-2xl bg-secondary">
+           
+                <h2 className="pt-2 font-sans font-semibold">Administrar usuarios</h2>
+                <p className="pt-2 pr-3 text-xs font-light"> Envía invitaciones a nuevos usuarios, modifica la información y roles existentes.</p>
+
                 <div className="flex justify-end pb-2 pr-3">
                     <Link href="/cms/users">
-                        <a className="pl-5 pr-5 mt-5 text-xs rounded-full bg-accent">Vamos allá</a>
-                    </Link> 
+                        <a className="pl-5 pr-5 font-medium text-xs mt-5 text-white rounded-full bg-accent">Vamos allá</a>
+                    </Link>
                 </div>
-            </div>
-            
-            <div className="pl-3 text-sm bg-white rounded-lg text-secondary">
-                <h2 className="font-sans font-semibold">Administracion de talleres sociofectivos</h2>
-                    <p className="mt-3 text-xs font-light"> Añade, modifica y elimina actividades. Consulta la información de un taller en específico</p>
-                
-                <div className="flex justify-end pr-3">
-                    <Link href="/cms/users">
-                        <a className="pl-5 pr-5 m-3 font-medium text-white rounded-full bg-accent">Vamos allá</a>
-                    </Link> 
-                </div>
-                
             </div>
 
-            <div className="flex p-5 text-sm text-white rounded-lg bg-secondary">
-                <div> <p>logo ceii</p></div>
-                
-                <div>    
-                <h2 className="font-sans font-semibold">Es lo único que puedes hacer por el momento...</h2>
-                <p className="mt-3 text-xs font-light">¡Agregaremos nuevas funciones pronto!</p>
+            <div className="pl-4 text-sm bg-white rounded-2xl text-secondary">
+                <h2 className="pt-2 font-sans font-semibold">Administrar talleres sociofectivos</h2>
+                <p className="mt-3 text-xs font-light"> Añade, modifica y elimina actividades. Consulta la información de un taller en específico</p>
+
+                <div className="flex justify-end pb-2 pr-3">
+                    <Link href="/cms/users">
+                        <a className="pl-5 pr-5 font-medium  text-xs mt-3 text-white rounded-full bg-accent">Vamos allá</a>
+                    </Link>
+                </div>
+
+            </div>
+
+            <div className="flex p-5 text-sm text-white rounded-2xl bg-secondary">
+                <div>
+                    <Cmslogo className="w-20 h-20 mr-3 " />
+                </div>
+            
+                <div>
+                    <h2 className="font-sans font-semibold">Es lo único que puedes hacer por el momento...</h2>
+                    <p className="mt-3 text-xs font-light">¡Agregaremos nuevas funciones pronto!</p>
                 </div>
             </div>
-            </section>
-        </div>
+        </section>
+    </div>
 )
 export default Cms;
