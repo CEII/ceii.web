0cca98e9fa7dd0485b3fbc23cd34ba7808e48211
Bryan31542 <00063820@uca.edu.sv>
2022-01-11 22:49:15 -0600

    feat: Creating search bar and buttons

--- pages/cms/activities/preu/index.tsx
+++ pages/cms/activities/preu/index.tsx
@@ -1,9 +1,11 @@
 import Layout from '@components/Layout';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faBars} from '@fortawesome/free-solid-svg-icons';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { SearchCircleIcon, PlusCircleIcon } from '@heroicons/react/solid';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import CenteredContainer from '@components/Containers/CenteredContainer';
+import InputGroup from '@components/Inputs/InputGroup';
 
 const Preu: NextPage = () => (
     <>
@@ -16,12 +18,36 @@ const Preu: NextPage = () => (
 
         <Layout showFooter>
             <CenteredContainer className="w-full h-full space-y-8" orientation="vertical">
-                <h1 className="font-sans text-white">Actividades</h1>
-
-
+                <div className="container w-4/5">
+                    <div className="flex flex-col">
+                        <h1 className="font-sans font-bold text-2xl text-white">Actividades</h1>
+                        <h2 className="font-sans text-xl italic text-white mb-4">
+                            Por ahora, solo del PreUniversitario
+                        </h2>
+                    </div>
+                    <div className="flex w-full">
+                        <div className="flex relative justify-center items-center w-full">
+                            <InputGroup
+                                className="font-sans w-full h-11 placeholder:text-left placeholder:italic"
+                                placeholder="Busca una actividad"
+                                type="text"
+                                identifier="activity"
+                                required
+                                minLength={19}
+                                maxLength={19}
+                                label={{ text: '' }}
+                            />
+                            <button type="button" className="rounded-full absolute right-0 mr-0.5">
+                                <SearchCircleIcon className="h-11 text-secondary font-bold" />
+                            </button>
+                        </div>
+                        <button type="button" className="bg-white rounded-full ml-2">
+                            <PlusCircleIcon className="h-11 p-1 text-secondary font-bold" />
+                        </button>
+                    </div>
+                </div>
             </CenteredContainer>
         </Layout>
-
     </>
 );
 
