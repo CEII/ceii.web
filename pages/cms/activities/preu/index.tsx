dc7610eaabe7c4096cd99597f97024addafc680b
FR0K3 <00174320@uca.edu.sv>
2022-01-11 21:17:39 -0600

    feat: Adding cms preu basic structure

--- pages/cms/activities/preu/index.tsx
+++ pages/cms/activities/preu/index.tsx
@@ -1,8 +1,28 @@
+import Layout from '@components/Layout';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faBars} from '@fortawesome/free-solid-svg-icons';
 import type { NextPage } from 'next';
+import Head from 'next/head';
+import CenteredContainer from '@components/Containers/CenteredContainer';
 
 const Preu: NextPage = () => (
-    <div>
-        <h1>admin actividades preu ruta:/cms/activities/preu</h1>
-    </div>
+    <>
+        <Head>
+            <title>Preu</title>
+        </Head>
+        <nav>
+            <FontAwesomeIcon icon={faBars} className="w-10 mt-3 ml-4 text-2xl text-white md:text-5xl" />
+        </nav>
+
+        <Layout showFooter>
+            <CenteredContainer className="w-full h-full space-y-8" orientation="vertical">
+                <h1 className="font-sans text-white">Actividades</h1>
+
+
+            </CenteredContainer>
+        </Layout>
+
+    </>
 );
+
 export default Preu;
