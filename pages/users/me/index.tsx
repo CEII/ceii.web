a637b40fb38a9e2e8af44502995e4c7eacea36ac
MarianaLagrava <marianalagrava@gmail.com>
2022-01-13 15:52:40 -0600

    feat:Added UserFooter component

--- pages/users/me/index.tsx
+++ pages/users/me/index.tsx
@@ -1,14 +1,13 @@
 import type { NextPage } from 'next';
-import { FC } from 'react';
 import Layout from '@components/Layout';
 import Head from 'next/head';
+import UserFooter from '@components/UserFooter';
 
 const SettingsUser: NextPage = () => (
     <>
         <Head>
             <title>Settings</title>
         </Head>    
-        
         <Layout showFooter showNav>
            
 
---
+++ src/components/UserFooter/UserFooter.tsx
@@ -0,0 +1,18 @@
+import { FC } from 'react';
+
+
+const UserFooter: FC = ()=>{
+
+    return(
+
+        <footer>
+            <div>
+                <h1>Hola</h1>
+                
+            </div>
+        </footer>
+    
+    )
+}
+
+export default UserFooter;
