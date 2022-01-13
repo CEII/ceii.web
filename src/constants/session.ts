8880db7d24fa002ea5fc2fe806fb0b1b2f5ed4ca
wmoralesdev <walterrafael26@gmail.com>
2022-01-13 01:17:55 -0600

    fix: api url

--- src/constants/session.ts
+++ src/constants/session.ts
@@ -3,4 +3,4 @@ export const USER_DISPLAY_NAME = 'displayName';
 export const USER_IMAGE = 'image';
 export const AUTH_TOKEN_ROLE = 'role';
 
-export const CORE_API_URL = 'https://localhost:7270';
+export const CORE_API_URL = 'https://ceii-api.herokuapp.com';
