c6fb51e632d4e3ece6df0936325c18b8943beb83
MarianaLagrava <marianalagrava@gmail.com>
2022-01-13 17:24:55 -0600

    Style: Style config page

--- pages/users/me/index.tsx
+++ pages/users/me/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next';
 import Layout from '@components/Layout';
 import Head from 'next/head';
 import UserFooter from '@components/UserFooter';
+ 
 
 const SettingsUser: NextPage = () => (
     <>
@@ -10,6 +11,14 @@ const SettingsUser: NextPage = () => (
         </Head>    
         <Layout showFooter showNav>
            
+                <div className="w-full bg-slate-600 text-center mb-0">
+                    <p className='text-white italic font-light text-xs'>Edita solo los campos que te gustaría modificar</p>
+                </div>
+           <section  className="mt-2  h-full bg-gray-50 flex flex-col items-center space-y-2 ">
+                <div className=" h-full w-3/4 rounded-md">
+                    <h2 className="text-secondary text-center font-bold text-sm mt-2"> Configuración de perfil</h2>
+                </div>
+           </section>
 
         </Layout>
 
