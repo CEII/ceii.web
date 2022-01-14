d2fbf882bf4313539f27b3ef8ccb09f9fddc2956
MJZF00155619 <00155619@uca.edu.sv>
2022-01-14 12:21:09 -0600

    style: add select input

--- pages/cms/activities/preu/edit/index.tsx
+++ pages/cms/activities/preu/edit/index.tsx
@@ -3,10 +3,8 @@ import Protected from '@components/Protected';
 import { GoogleProps } from 'interfaces/props';
 import { useSession } from 'next-auth/react';
 import Head from 'next/head';
-import Link from 'next/link';
 import React from 'react';
 import type { NextPage } from 'next';
-import CenteredContainer from '@components/Containers/CenteredContainer';
 import InputGroup from '@components/Inputs/InputGroup';
 import { ExclamationCircleIcon } from "@heroicons/react/solid";
 import { Fragment, useState } from 'react'
@@ -14,12 +12,12 @@ import { Listbox, Transition } from '@headlessui/react'
 import { CheckIcon, SelectorIcon } from '@heroicons/react/solid'
 
 const modalities = [
-    { id: 1, modality: 'Virtual' },
-    { id: 2, modality: 'Presencial' },
+    { name: 'Virtual' },
+    { name: 'Presencial' },
 ]
 const New: NextPage<GoogleProps> = () => {
 
-    const [selectedModality, setSelectedModality] = useState(modalities[0])
+    const [selected, setSelected] = useState(modalities[0])
 
     const { data: session } = useSession();
 
@@ -107,26 +105,60 @@ const New: NextPage<GoogleProps> = () => {
 
                                         <div className='flex flex-col'>
                                             <label className='xl:text-lg text-white font-bold'>Modalidad</label>
-                                            <Listbox value={selectedModality} onChange={setSelectedModality}>
-                                                <Listbox.Button className="relative xl:w-2/3 xl:h-10 text-left bg-white rounded-full shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 xl:text-xl">{selectedModality.modality}
-                                                    <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-                                                        <SelectorIcon
-                                                            className="w-5 h-5 text-gray-400"
-                                                            aria-hidden="true"
-                                                        />
-                                                    </span>
-                                                </Listbox.Button>
-                                                <Listbox.Options className="bg-white rounded-xl mt-2">
-                                                    {modalities.map((modality) => (
-                                                        <Listbox.Option
-                                                            key={modality.id}
-                                                            value={modality}
-                                                        >
-                                                            {modality.modality}
-                                                        </Listbox.Option>
-                                                    ))}
-                                                </Listbox.Options>
+
+                                            <Listbox value={selected} onChange={setSelected}>
+                                                <div className="relative mt-1">
+                                                    <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-full shadow-md cursor-default focus:outline-none focus-visible:ring-2 focus-visible:ring-opacity-75 focus-visible:ring-white focus-visible:ring-offset-orange-300 focus-visible:ring-offset-2 focus-visible:border-indigo-500 sm:text-sm">
+                                                        <span className="block truncate">{selected.name}</span>
+                                                        <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                                                            <SelectorIcon
+                                                                className="w-5 h-5 text-gray-400"
+                                                                aria-hidden="true"
+                                                            />
+                                                        </span>
+                                                    </Listbox.Button>
+                                                    <Transition
+                                                        as={Fragment}
+                                                        leave="transition ease-in duration-100"
+                                                        leaveFrom="opacity-100"
+                                                        leaveTo="opacity-0"
+                                                    >
+                                                        <Listbox.Options className="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                                                            {modalities.map((modalities, modalitiesIdx) => (
+                                                                <Listbox.Option
+                                                                    key={modalitiesIdx}
+                                                                    className={({ active }) =>
+                                                                        `${active ? 'text-amber-900 bg-amber-100' : 'text-gray-900'}
+                                                                        cursor-default select-none relative py-2 pl-10 pr-4`
+                                                                    }
+                                                                    value={modalities}
+                                                                >
+                                                                    {({ selected, active }) => (
+                                                                        <>
+                                                                            <span
+                                                                                className={`${selected ? 'font-medium' : 'font-normal'
+                                                                                    } block truncate`}
+                                                                            >
+                                                                                {modalities.name}
+                                                                            </span>
+                                                                            {selected ? (
+                                                                                <span
+                                                                                    className={`${active ? 'text-amber-600' : 'text-amber-600'
+                                                                                        }
+                                                                                        absolute inset-y-0 left-0 flex items-center pl-3`}
+                                                                                >
+                                                                                    <CheckIcon className="w-5 h-5" aria-hidden="true" />
+                                                                                </span>
+                                                                            ) : null}
+                                                                        </>
+                                                                    )}
+                                                                </Listbox.Option>
+                                                            ))}
+                                                        </Listbox.Options>
+                                                    </Transition>
+                                                </div>
                                             </Listbox>
+
                                         </div>
 
                                     </div>
