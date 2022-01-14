a9f3d6985509a5006dd428f66bb7b389e6645d1a
marcelob12 <00080119@uca.edu.sv>
2022-01-14 13:55:00 -0600

    style:add pencil component & edit buttom responsive

--- pages/users/me/index.tsx
+++ pages/users/me/index.tsx
@@ -5,6 +5,8 @@ import { ImageContainer } from '@components/ImageContainer';
 import InputGroup from '@components/Inputs/InputGroup';
 import { FC } from 'react';
 import UserFooter from '@components/UserFooter';
+import Pencil from '@components/Icons/Penciledit';
+
 
 const InputContainer:FC =({children})=>(
     <div className="flex flex-col-reverse space-y-2 space-y-reverse text-secondary font-medium group">
@@ -30,9 +32,16 @@ const SettingsUser: NextPage = () => (
                 <div className="ring-2 ring-white bg-white  w-5/6 rounded-t-xl flex flex-col items-center lg:h-3/4 md:h-full">
                   
                     <div className="flex flex-col h-5/6 justify-center lg:flex-row lg:w-10/12 lg:gap-20" >
-                        <div className="mt-14 lg:mt-20 lg:mx-3">
-                        <h2 className="text-secondary text-center font-bold text-lg "> Configuración de perfil</h2>
-                        <ImageContainer src="/img/empty.png" alt='Profile picture' className="w-28 h-28 mt-3 ring-4 rounded-full ring-accent mx-auto lg:w-56 lg:h-56 lg:self-center" rounded />
+                        <div className="mt-16 lg:mt-20 lg:mx-3">
+                            <h2 className="text-secondary text-center font-bold text-lg mb-8 "> Configuración de perfil</h2>
+
+                            <ImageContainer src="/img/empty.png" alt='Profile picture' className=" w-28 h-28 mt-3 ring-4 rounded-full ring-accent mx-auto lg:w-56 lg:h-56 lg:self-center " rounded />
+
+                            <div className=' relative bottom-32 left-40 z-10 group lg:bottom-60 lg:left-48 '>
+                                <button className="  w-9 h-6 px-3  bg-gray-300 rounded-full   font-bold text-xs group-hover:bg-slate-500 group-hover:text-slate-600 "><Pencil className='w-4 h-4'/> </button>
+
+                            </div>
+
                         </div>
                         
                         <form className="mt-5 h-3/4 text-center flex flex-col space-y-2 text-secondary font-medium lg:w-6/12 lg:m-14">
