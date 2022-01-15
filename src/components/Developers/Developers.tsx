9c194444fc2408fe8613c801554b19e8ec0fad7d
Manuel Madriz <00026220@uca.edu.sv>
2022-01-15 00:05:47 -0600

    feat: connect developers to Api

--- src/components/Developers/Developers.tsx
+++ src/components/Developers/Developers.tsx
@@ -1,9 +1,11 @@
 import CenteredContainer from '@components/Containers/CenteredContainer';
 import { ImageContainer } from '@components/ImageContainer';
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useEffect } from 'react';
 import { FontAwesomeIcon as Icon } from '@fortawesome/react-fontawesome';
 import { faInstagram, faLinkedin, faGithub, faTwitter, faFacebook } from '@fortawesome/free-brands-svg-icons';
 import { CardTeamProps } from '@interfaces/props';
+import { useQuery} from 'react-query';
+import { getDevs } from '@services/devs/devService';
 
 const CardTeam: FunctionComponent<CardTeamProps> = ({ email, name, lastName, imgUrl, participation }) => {
     return (
@@ -27,8 +29,8 @@ const CardTeam: FunctionComponent<CardTeamProps> = ({ email, name, lastName, img
         </CenteredContainer>
         <CenteredContainer orientation="vertical" className="font-bold text-sm space-y-2">
             {
-                participation.map( ptn => (
-                    <span className="px-2 py-1 bg-white rounded-full text-secondary">{ ptn }</span>
+                participation.map( (ptn, index) => (
+                    <span key={index} className="px-2 py-1 bg-white rounded-full text-secondary">{ ptn }</span>
                     )
                 )
             }
@@ -53,35 +55,41 @@ const CardTeam: FunctionComponent<CardTeamProps> = ({ email, name, lastName, img
     </CenteredContainer>)
 };
 
-const Team: FunctionComponent = () => (
-    <div className="w-full">
-        <div className="flex flex-col p-4 md:px-12 lg:py-16 lg:px-32 xl:p-16 xl:items-center">
-            <div className="py-6 px-3 md:py-10">
-                <h1 className="text-white font-bold text-2xl md:text-xl lg:text-2xl">
-                    Conoce a nuestros desarrolladores
-                </h1>
-                <span className="text-white italic text-md md:text-base lg:text-xl">
-                    ¡Gracias a todos por su contribución!
-                </span>
-            </div>
-            {/* Cards-container */}
-            <div className="flex flex-wrap items-center justify-center text-white px-2">
-                <CardTeam
-                    email={"00019618@uca.edu.sv"}
-                    name={"Walter"}
-                    lastName={"Morales"}
-                    imgUrl={"https://ceii-s3.s3.amazonaws.com/users/202201132136568806.jpg"}
-                    participation={["Lead Programmer", "Lead Programmer", "Lead Programmer"]}/>
-                
-                <CardTeam
-                    email={"00019618@uca.edu.sv"}
-                    name={"Walter"}
-                    lastName={"Morales"}
-                    imgUrl={"https://ceii-s3.s3.amazonaws.com/users/202201132136568806.jpg"}
-                    participation={["Lead Programmer", "Lead Programmer"]}/>
+const Team: FunctionComponent = () => {
+    const { data, refetch } = useQuery('developers', () => getDevs());
+
+    useEffect(() => {
+        refetch();
+    }, []);
+
+    return (
+        <div className="w-full">
+            <div className="flex flex-col p-4 md:px-12 lg:py-16 lg:px-32 xl:p-16 xl:items-center">
+                <div className="py-6 px-3 md:py-10">
+                    <h1 className="text-white font-bold text-2xl md:text-xl lg:text-2xl">
+                        Conoce a nuestros desarrolladores
+                    </h1>
+                    <span className="text-white italic text-md md:text-base lg:text-xl">
+                        ¡Gracias a todos por su contribución!
+                    </span>
+                </div>
+                {/* Cards-container */}
+                <div className="flex flex-wrap items-center justify-center text-white px-2">
+
+                    { data && data.developers.map(({ user, participation }) => (
+                        user &&
+                        <CardTeam
+                            key={user.email}
+                            email={user.email}
+                            name={user.name}
+                            lastName={user.lastName}
+                            imgUrl={user.imageUrl}
+                            participation={participation.split(", ")}/>
+                    ))}
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+}
 
 export default Team;
